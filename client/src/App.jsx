96e02e205aad1fac79e5574c04a952d609e9ddac
Tejasvibihari <tejasvibihari2000@gmail.com>
2024-06-18 15:07:00 +0530

    Added Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import SignIn from './pages/SignIn'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ResetPasswordForm from './components/adminAuth/AdminResetPasswordForm'
 import VerifyPasswordForm from './components/adminAuth/AdminVerifyPasswordForm'
+import Home from './pages/Home'
 
 
 
@@ -10,7 +11,7 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-     
+
         <Routes>
           {/* Admin Authentication Start */}
           <Route path="/signup" element={<SignUp />} />
@@ -18,6 +19,8 @@ export default function App() {
           <Route path="/resetpassword" element={<ResetPasswordForm />} />
           <Route path="/verifypassword" element={<VerifyPasswordForm />} />
           {/* Admin Authentication End */}
+          <Route path="/" element={<Home />} />
+
         </Routes>
       </BrowserRouter>
     </>
