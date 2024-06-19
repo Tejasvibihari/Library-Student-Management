e3f95c959c34f8eb422c0093d9c8670991032689
Tejasvibihari <tejasvibihari2000@gmail.com>
2024-06-19 23:57:30 +0530

    Editor Installed and checked

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,22 @@
-
-import SideBar from "../components/Sidebar"
-
+import React, { useState, useRef } from 'react';
+import SideBar from "../components/Sidebar";
+import JoditEditor from 'jodit-react';
 
 export default function Home() {
+    const editor = useRef(null);
+    const [content, setContent] = useState('');
+
+    const contentFieldChanged = (newContent) => setContent(newContent);
+
     return (
         <>
             <SideBar>
-                Home
+                <JoditEditor
+                    ref={editor}
+                    value={content}
+                    onChange={contentFieldChanged}
+                />
             </SideBar>
         </>
-    )
+    );
 }
