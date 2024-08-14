6e14d4b47faaeda7f41b262eea72c915d07a91df
Tejasvibihari <tejasvibihari2000@gmail.com>
2024-08-14 16:36:23 +0530

    Mail ui change

--- client/src/components/EmailEditor.jsx
+++ client/src/components/EmailEditor.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import JoditEditor from 'jodit-react';
-import SideBar from './Sidebar';
+// import SideBar from './Sidebar';
 import Breadcrumbs from './Breadcrumbs';
 import client from '../services/axiosClient';
 import { useSelector } from 'react-redux';
@@ -54,7 +54,7 @@ export default function EmailEditor() {
     }, [adminId])
 
     // Send Email
-  
+
     const handleClick = async (e) => {
         e.preventDefault()
         // setContent(editorContent)
@@ -93,72 +93,72 @@ export default function EmailEditor() {
     return (
         <div>
 
-            <SideBar >
-                <Snackbar open={snackOpen} autoHideDuration={6000} onClose={handleSnackClose}>
-                    <Alert
-                        onClose={handleSnackClose}
-                        severity={alertStatus === "Email sent successfully" ? "success" : "error"}
-                        variant="filled"
-                        sx={{ width: '100%' }}
-                    >
-                        {alertStatus}
-                    </Alert>
-                </Snackbar>
-                <Breadcrumbs title="Compose Email" subTitle='Announcment' />
-                <div className='shadow-lg p-4 rounded-md'>
-                    <form>
-                        <div className='flex justify-between mb-4 gap-4'>
-                            <div className='w-full'>
-                                <label htmlFor="library-membership">To:</label>
-                                <input
-                                    className='border rounded-lg w-full p-1'
-                                    list="membership-types"
-                                    name="library-membership"
-                                    placeholder="To"
-                                    value={to}
-                                    onChange={(e) => setTo(e.target.value)}
-                                />
-                                <datalist id="membership-types">
-                                    <option value="All"></option>
-                                    {emails.map((email, i) => {
-                                        return <option key={i} value={email}></option>
-                                    })}
-                                </datalist>
-                            </div>
-                            <div className='w-full'>
-                                <label htmlFor="subject">Subject:</label>
-                                <input
-                                    className='border rounded-lg w-full p-1'
-                                    type="text"
-                                    id="subject"
-                                    name="subject"
-                                    placeholder='Subject'
-                                    value={subject}
-                                    onChange={(e) => setSubject(e.target.value)}
-                                    required />
-                            </div>
-                        </div>
-                        <div>
-                            <JoditEditor
-                                ref={editor}
-                                value={content}
-                                onChange={handleEditorChange}
-                                config={{
-                                    height: 350 // Set the height to 400px or any other value as needed
-                                }}
+            {/* <SideBar > */}
+            <Snackbar open={snackOpen} autoHideDuration={6000} onClose={handleSnackClose}>
+                <Alert
+                    onClose={handleSnackClose}
+                    severity={alertStatus === "Email sent successfully" ? "success" : "error"}
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    {alertStatus}
+                </Alert>
+            </Snackbar>
+            <Breadcrumbs title="Compose Email" subTitle='Announcment' />
+            <div className='shadow-lg p-4 rounded-md'>
+                <form>
+                    <div className='flex justify-between mb-4 gap-4'>
+                        <div className='w-full'>
+                            <label htmlFor="library-membership">To:</label>
+                            <input
+                                className='border rounded-lg w-full p-1'
+                                list="membership-types"
+                                name="library-membership"
+                                placeholder="To"
+                                value={to}
+                                onChange={(e) => setTo(e.target.value)}
                             />
+                            <datalist id="membership-types">
+                                <option value="All"></option>
+                                {emails.map((email, i) => {
+                                    return <option key={i} value={email}></option>
+                                })}
+                            </datalist>
                         </div>
-                        <div className='flex justify-end my-6'>
-                            <button onClick={handleClick} type='submit' className='p-2 w-md border rounded-md flex justify-center items-center text-white bg-[#8e54e9] hover:bg-[#8e54e9e6]'>
+                        <div className='w-full'>
+                            <label htmlFor="subject">Subject:</label>
+                            <input
+                                className='border rounded-lg w-full p-1'
+                                type="text"
+                                id="subject"
+                                name="subject"
+                                placeholder='Subject'
+                                value={subject}
+                                onChange={(e) => setSubject(e.target.value)}
+                                required />
+                        </div>
+                    </div>
+                    <div>
+                        <JoditEditor
+                            ref={editor}
+                            value={content}
+                            onChange={handleEditorChange}
+                            config={{
+                                height: 350 // Set the height to 400px or any other value as needed
+                            }}
+                        />
+                    </div>
+                    <div className='flex justify-end my-6'>
+                        <button onClick={handleClick} type='submit' className='p-2 w-md border rounded-md flex justify-center items-center text-white bg-[#8e54e9] hover:bg-[#8e54e9e6]'>
 
-                                {loading ? <div className='flex items-center justify-center'><span className='mr-2'>Please Wait..</span><CircularLoading size={25} /></div> :
-                                    <div className='flex items-center'>
-                                        <Forward size={17} className='mr-2' />Send Email</div>}
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </SideBar >
+                            {loading ? <div className='flex items-center justify-center'><span className='mr-2'>Please Wait..</span><CircularLoading size={25} /></div> :
+                                <div className='flex items-center'>
+                                    <Forward size={17} className='mr-2' />Send Email</div>}
+                        </button>
+                    </div>
+                </form>
+            </div>
+            {/* </SideBar > */}
         </div >
     )
 }
