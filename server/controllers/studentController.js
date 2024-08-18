d46ec266308e1d3fc3f3031c20a08d4eaff75f29
Tejasvibihari <tejasvibihari2000@gmail.com>
2024-08-18 21:32:21 +0530

    update api

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -20,8 +20,6 @@ export const createStudent = async (req, res) => {
         father,
         guardian,
         gender,
-
-
         admissionDate,
         shift,
         time,
@@ -87,38 +85,38 @@ export const createStudent = async (req, res) => {
                         const hashedPassword = await bcrypt.hash(password, 12);
 
                         await Student.create({
-                            sid, name, dob, email, seatNumber, password: hashedPassword, mobile, father, guardian,
+                            sid, name, email, seatNumber, password: hashedPassword, mobile, father, guardian,
                             gender, admissionDate, shift, time, paymentAmount, address,
                             image: imageFilename, lastPayment
                         });
                         await seat.save();
-                        sendMail({
-                            to: email,
-                            subject: "Welcome to Bihari Library - Admission Confirmation",
-                            body: `
-<p>Dear ${name},</p>
-<p>We are thrilled to welcome you to Bihari Library Congratulations on your admission. Below are the details of your enrollment:</p>
-<p><strong>Student Information:</strong></p>
-<ul>
-<li><strong>Student ID:</strong> ${sid}</li>
-<li><strong>Name:</strong> ${name}</li>
-<li><strong>Shift:</strong> ${shift}</li>
-<li><strong>Admission Date:</strong> ${admissionDate}</li>
-<li><strong>Father's Name:</strong> ${father}</li>
-<li><strong>Address:</strong> ${address}</li>
-<li><strong>Email:</strong> ${email}</li>
-<li><strong>Password:</strong> ${password}</li>
-</ul>
-<p>To complete your admission, please proceed with the payment of the required fee. Detailed payment instructions and deadlines will be sent to you soon.</p>
-<p>You can access your student dashboard and manage your account by clicking the link below: <a href="https://biharilibrary.in/student-dashboard" target="_new" rel="noreferrer">Student Dashboard</a></p>
-<p>or&nbsp;</p>
-<p>Copy the link Given Below and paste it into any Browser</p>
-<p>https://biharilibrary.in/student-dashboard</p>
-<p>Should you have any questions or need further assistance, feel free to reach out to us at <strong>Bihari Library</strong>. We are here to support you every step of the way.</p>
-<p>Once again, congratulations on your admission! We look forward to having you join our vibrant campus community.</p>
-<p>Warm regards,</p>
-<p><img src="https://marudhardentalclinic.com/wp-content/uploads/2024/08/20240811_173606-scaled.webp" alt="bihari logo" width="150" height="50" /><br /><em>9608888400</em></p>`
-                        });
+                        //                         sendMail({
+                        //                             to: email,
+                        //                             subject: "Welcome to Bihari Library - Admission Confirmation",
+                        //                             body: `
+                        // <p>Dear ${name},</p>
+                        // <p>We are thrilled to welcome you to Bihari Library Congratulations on your admission. Below are the details of your enrollment:</p>
+                        // <p><strong>Student Information:</strong></p>
+                        // <ul>
+                        // <li><strong>Student ID:</strong> ${sid}</li>
+                        // <li><strong>Name:</strong> ${name}</li>
+                        // <li><strong>Shift:</strong> ${shift}</li>
+                        // <li><strong>Admission Date:</strong> ${admissionDate}</li>
+                        // <li><strong>Father's Name:</strong> ${father}</li>
+                        // <li><strong>Address:</strong> ${address}</li>
+                        // <li><strong>Email:</strong> ${email}</li>
+                        // <li><strong>Password:</strong> ${password}</li>
+                        // </ul>
+                        // <p>To complete your admission, please proceed with the payment of the required fee. Detailed payment instructions and deadlines will be sent to you soon.</p>
+                        // <p>You can access your student dashboard and manage your account by clicking the link below: <a href="https://biharilibrary.in/student-dashboard" target="_new" rel="noreferrer">Student Dashboard</a></p>
+                        // <p>or&nbsp;</p>
+                        // <p>Copy the link Given Below and paste it into any Browser</p>
+                        // <p>https://biharilibrary.in/student-dashboard</p>
+                        // <p>Should you have any questions or need further assistance, feel free to reach out to us at <strong>Bihari Library</strong>. We are here to support you every step of the way.</p>
+                        // <p>Once again, congratulations on your admission! We look forward to having you join our vibrant campus community.</p>
+                        // <p>Warm regards,</p>
+                        // <p><img src="https://marudhardentalclinic.com/wp-content/uploads/2024/08/20240811_173606-scaled.webp" alt="bihari logo" width="150" height="50" /><br /><em>9608888400</em></p>`
+                        //                         });
 
                         return res.status(201).json({ message: "Admission Success" });
                     } else {
@@ -204,33 +202,33 @@ export const createStudent = async (req, res) => {
                             image: imageFilename, lastPayment
                         });
                         await seat.save();
-                        sendMail({
-                            to: email,
-                            subject: "Welcome to Bihari Library - Admission Confirmation",
-                            body: `<hr />
-<p>Dear ${name},</p>
-<p>We are thrilled to welcome you to Bihari Library Congratulations on your admission. Below are the details of your enrollment:</p>
-<p><strong>Student Information:</strong></p>
-<ul>
-<li><strong>Student ID:</strong> ${newSid}</li>
-<li><strong>Name:</strong> ${name}</li>
-<li><strong>Shift:</strong> ${shift}</li>
-<li><strong>Admission Date:</strong> ${admissionDate}</li>
-<li><strong>Father's Name:</strong> ${father}</li>
-<li><strong>Address:</strong> ${address}</li>
-<li><strong>Email:</strong> ${email}</li>
-<li><strong>Password:</strong> ${password}</li>
-</ul>
-<p>To complete your admission, please proceed with the payment of the required fee. Detailed payment instructions and deadlines will be sent to you soon.</p>
-<p>You can access your student dashboard and manage your account by clicking the link below: <a href="https://biharilibrary.in/student-dashboard" target="_new" rel="noreferrer">Student Dashboard</a></p>
-<p>or&nbsp;</p>
-<p>Copy the link Given Below and paste it into any Browser</p>
-<p>https://biharilibrary.in/student-dashboard</p>
-<p>Should you have any questions or need further assistance, feel free to reach out to us at <strong>Bihari Library</strong>. We are here to support you every step of the way.</p>
-<p>Once again, congratulations on your admission! We look forward to having you join our vibrant campus community.</p>
-<p>Warm regards,</p>
-<p><img src="https://marudhardentalclinic.com/wp-content/uploads/2024/08/20240811_173606-scaled.webp" alt="bihari logo" width="150" height="50" /><br /><em>9608888400</em></p>`
-                        });
+                        //                         sendMail({
+                        //                             to: email,
+                        //                             subject: "Welcome to Bihari Library - Admission Confirmation",
+                        //                             body: `<hr />
+                        // <p>Dear ${name},</p>
+                        // <p>We are thrilled to welcome you to Bihari Library Congratulations on your admission. Below are the details of your enrollment:</p>
+                        // <p><strong>Student Information:</strong></p>
+                        // <ul>
+                        // <li><strong>Student ID:</strong> ${newSid}</li>
+                        // <li><strong>Name:</strong> ${name}</li>
+                        // <li><strong>Shift:</strong> ${shift}</li>
+                        // <li><strong>Admission Date:</strong> ${admissionDate}</li>
+                        // <li><strong>Father's Name:</strong> ${father}</li>
+                        // <li><strong>Address:</strong> ${address}</li>
+                        // <li><strong>Email:</strong> ${email}</li>
+                        // <li><strong>Password:</strong> ${password}</li>
+                        // </ul>
+                        // <p>To complete your admission, please proceed with the payment of the required fee. Detailed payment instructions and deadlines will be sent to you soon.</p>
+                        // <p>You can access your student dashboard and manage your account by clicking the link below: <a href="https://biharilibrary.in/student-dashboard" target="_new" rel="noreferrer">Student Dashboard</a></p>
+                        // <p>or&nbsp;</p>
+                        // <p>Copy the link Given Below and paste it into any Browser</p>
+                        // <p>https://biharilibrary.in/student-dashboard</p>
+                        // <p>Should you have any questions or need further assistance, feel free to reach out to us at <strong>Bihari Library</strong>. We are here to support you every step of the way.</p>
+                        // <p>Once again, congratulations on your admission! We look forward to having you join our vibrant campus community.</p>
+                        // <p>Warm regards,</p>
+                        // <p><img src="https://marudhardentalclinic.com/wp-content/uploads/2024/08/20240811_173606-scaled.webp" alt="bihari logo" width="150" height="50" /><br /><em>9608888400</em></p>`
+                        //                         });
 
                         return res.status(201).json({ message: "Admission Success" });
                     } else {
